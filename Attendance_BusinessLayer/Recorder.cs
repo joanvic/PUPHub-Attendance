2105d9eb9be12eac46a1e4b0a82f3e85a49eb8c5
Nico Ampoloquio <116412203+NicoAmpoloquio@users.noreply.github.com>
2023-05-25 15:08:20 +0800

    Initial commit

---
+++ Attendance_BusinessLayer/Recorder.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using Attendance_DataLayer;
+using Attendance_Models;
+
+namespace Attendance_BusinessLayer
+{
+    public class AttendanceManager
+    {
+        public static void RecordAttendance()
+        {
+            Console.Write("Enter student name: ");
+            string studentName = Console.ReadLine();
+
+            StudentAttendanceRecord studentRecord = Students.StudentList.Find(record => record.StudentName == studentName);
+
+            if (studentRecord == null)
+            {
+                Console.WriteLine("Invalid student name! Attendance not marked.");
+                return;
+            }
+
+            Console.WriteLine("Select attendance status:");
+            Console.WriteLine("1. Present");
+            Console.WriteLine("2. Absent");
+            Console.WriteLine("3. Excused");
+            Console.Write("Enter your choice: ");
+            string statusChoice = Console.ReadLine();
+
+            string attendanceStatus;
+
+            switch (statusChoice)
+            {
+                case "1":
+                    attendanceStatus = "Present";
+                    break;
+                case "2":
+                    attendanceStatus = "Absent";
+                    break;
+                case "3":
+                    attendanceStatus = "Excused";
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice! Attendance not marked.");
+                    return;
+            }
+
+            DateTime currentTime = DateTime.Now;
+
+            studentRecord.StudentList.Add(new RecordDateTime(currentTime, attendanceStatus));
+            Console.WriteLine("Attendance marked successfully.");
+        }
+        public static void ViewAttendanceRecordsByStudent()
+        {
+            Console.Write("Enter student name: ");
+            string studentName = Console.ReadLine();
+
+            StudentAttendanceRecord studentRecord = Students.StudentList.Find(record => record.StudentName == studentName);
+
+            if (studentRecord == null)
+            {
+                Console.WriteLine("No attendance records found for this student.");
+                return;
+            }
+
+            Console.WriteLine($"Attendance records for {studentName}:");
+            foreach (var record in studentRecord.StudentList)
+            {
+                Console.WriteLine($"Date: {record.Time.ToShortDateString()}, Time: {record.Time.ToShortTimeString()}, Status: {record.Status}");
+            }
+        }
+        public static void ViewAttendanceRecordsForAllStudents()
+        {
+            Console.WriteLine("Attendance records for all students:");
+
+            if (Students.StudentList.Count == 0)
+            {
+                Console.WriteLine("No attendance records found for any student.");
+                return;
+            }
+
+            foreach (var student in Students.StudentList)
+            {
+                Console.WriteLine($"Student: {student.StudentName}");
+                foreach (var record in student.StudentList)
+                {
+                    Console.WriteLine($"Date: {record.Time.ToShortDateString()}, Time: {record.Time.ToShortTimeString()}, Status: {record.Status}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
---
+++ Attendance_DataLayer/InMemoryData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Attendance_Models;
+
+namespace Attendance_DataLayer
+{
+    public class Students
+    {
+        public static List<StudentAttendanceRecord> StudentList = new List<StudentAttendanceRecord>();
+        public static void InMemoryDataStudents()
+        {
+            StudentList.Add(new StudentAttendanceRecord("Nico Ampoloquio"));
+            StudentList.Add(new StudentAttendanceRecord("Joanvic Vargas"));
+            StudentList.Add(new StudentAttendanceRecord("Mekaila Aguila"));
+        }
+    }
+}
