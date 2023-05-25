b510db9f15a740ba73e918dd9dd401a33fb67146
Nico Ampoloquio <116412203+NicoAmpoloquio@users.noreply.github.com>
2023-05-25 22:32:00 +0800

    Update MainProgram.cs

--- MainProgram.cs
+++ MainProgram.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Attendance_BusinessLayer;
 using Attendance_DataLayer;
+using Attendance_UserInterface;
 
 namespace Attendance
 {
@@ -9,20 +10,22 @@ namespace Attendance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Attendance is a Must, attendance muna bago umalis mga ya.");
-            Console.WriteLine("-------------------------------------------");
+            UserInterfaceMenu.DisplayMainTitle();
+            //Console.WriteLine("Attendance is a Must, attendance muna bago umalis mga ya.");
+            //Console.WriteLine("-------------------------------------------");
 
             Students.InMemoryDataStudents();
 
             while (true)
             {
-                Console.WriteLine("\nPlease select an option:");
-                Console.WriteLine("1. Record attendance");
-                Console.WriteLine("2. View attendance records by student");
-                Console.WriteLine("3. View attendance records for all students");
-                Console.WriteLine("4. Edit attendance records");
-                Console.WriteLine("5. Exit");
-                Console.Write("Enter your choice: ");
+                UserInterfaceMenu.DisplayMainMenu();
+                //Console.WriteLine("\nPlease select an option:");
+                //Console.WriteLine("1. Record attendance");
+                //Console.WriteLine("2. View attendance records by student");
+                //Console.WriteLine("3. View attendance records for all students");
+                //Console.WriteLine("4. Edit attendance records");
+                //Console.WriteLine("5. Exit");
+                //Console.Write("Enter your choice: ");
 
                 string input = Console.ReadLine();
                 Console.WriteLine();
@@ -42,10 +45,10 @@ namespace Attendance
                         AttendanceManager.EditAttendance();
                         break;
                     case "5":
-                        Console.WriteLine("Bounce program na par...");
+                        UserInterfaceMenu.DisplayExitMessage();
                         return;
                     default:
-                        Console.WriteLine("Invalid choice, please try again.");
+                        UserInterfaceMenu.DisplayInvalidChoiceMessage();
                         break;
                 }
             }
